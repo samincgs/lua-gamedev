d347fa2eedc0e06857e7d41d182405e38f930e74
Samin <samincgs@gmail.com>
2025-02-26 23:12:25 -0500

    started the shooting gallery game

---
+++ shooting-gallery/main.lua
@@ -0,0 +1,21 @@
+function love.load()
+    target = {}
+    target.x = 0
+    target.y = 0
+    target.radius = 50
+
+    score = 0
+    timer = 0
+
+end
+
+
+function love.update(dt)
+
+end
+
+
+function love.draw()
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle('fill', target.x, target.y, target.radius)
+end
