8d9ad108264ec1d931efe6cc83e77c4c8de8a92d
Samin <samincgs@gmail.com>
2025-03-14 23:01:35 -0400

    created different files for better code management + added sprites using anim8 + added animation states + added collision boxes and jumps

--- boy-platformer/main.lua
+++ boy-platformer/main.lua
@@ -1,17 +1,32 @@
 function love.load()
-    wf = require 'libraries/windfield/windfield'
+    anim8 = require 'libraries/anim8/anim8'
+    wf = require 'libraries/windfield'
+
+    
+
+    sprites = {}
+    sprites.playerSheet = love.graphics.newImage('images/playerSheet.png')
+
+    local columns = 15
+    local rows = 3
+    local grid = anim8.newGrid(sprites.playerSheet:getWidth() / columns, sprites.playerSheet:getHeight() / rows, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+
+    animations = {}
+    animations.idle = anim8.newAnimation(grid('1-15', 1), 0.05)
+    animations.jump = anim8.newAnimation(grid('1-7', 2), 0.05)
+    animations.run = anim8.newAnimation(grid('1-15', 3), 0.05)
+
+
     -- first created a new World for all the physics objects
     local gravityY = 500
     world = wf.newWorld(0, gravityY, false) -- parameters: gravityX, gravityY, sleepMode
+    world:setQueryDebugDrawing(true) -- for debugging
 
     world:addCollisionClass('Platform')
     world:addCollisionClass('Player') --[[{ignores = {'Platform'}}]]
     world:addCollisionClass('Danger')
 
-     -- Physics object that contains information about of all funtions of the Love physics Body, Fixture and Shape parameters: x,y, width, height, table with collision class
-    player = world:newRectangleCollider(360, 100, 80, 80, {collision_class='Player'})
-    player:setFixedRotation(true)
-    player.speed = 240
+    require('player')
 
     platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class='Platform'})
     platform:setType('static')
@@ -23,28 +38,27 @@ end
 
 function love.update(dt)
     world:update(dt)
-
-    if player.body then
-        local px, py = player:getPosition()
-        if love.keyboard.isDown('right') then
-            player:setX(px + player.speed * dt)
-        end
-        if love.keyboard.isDown('left') then
-            player:setX(px - player.speed * dt)
-        end
-
-        if player:enter('Danger') then
-            player:destroy()
-        end
-    end
+    player:update(dt)
+    
 end
 
 function love.draw()
     world:draw()
+    player:draw()
+
 end
 
 function love.keypressed(key) 
     if key == 'up' then
-        player:applyLinearImpulse(0, -5000)
+        player:jump()
     end
 end
+
+-- function love.mousepressed(x, y, button)
+--     if button == 1 then
+--         local colliders = world:queryCircleArea(x, y, 200, {'Platform', 'Danger'})
+--         for i, c in ipairs(colliders) do
+--             c:destroy()
+--         end
+--     end
+-- end
---
+++ boy-platformer/player.lua
@@ -0,0 +1,57 @@
+player = world:newRectangleCollider(360, 100, 40, 100, {collision_class='Player'})
+player:setFixedRotation(true)
+player.speed = 240
+player.animation = animations.idle
+player.isMoving = false
+player.grounded = true
+player.direction = 1
+
+function player:update(dt)
+    if player.body then
+        player.isMoving = false
+        local px, py = player:getPosition()
+        local colliders = world:queryRectangleArea(px - 20, py + 50, 40, 4, {'Platform'})
+        if #colliders > 0 then
+            player.grounded = true
+        else
+            player.grounded = false
+        end
+
+        if love.keyboard.isDown('right') then
+            player:setX(px + player.speed * dt)
+            player.isMoving = true
+            player.direction = 1
+        end
+        if love.keyboard.isDown('left') then
+            player:setX(px - player.speed * dt)
+            player.isMoving = true
+            player.direction = -1
+        end
+
+        if player:enter('Danger') then
+            player:destroy()
+        end
+    end
+
+    if player.grounded then
+        if player.isMoving then
+            player.animation = animations.run
+        else
+            player.animation = animations.idle
+        end
+    else
+        player.animation = animations.jump
+    end
+
+    player.animation:update(dt)
+end
+
+function player:draw() 
+    player.animation:draw(sprites.playerSheet, player:getX(), player:getY(), nil, 0.25 * player.direction, 0.25, 130, 300)
+end
+
+function player:jump()
+    if player.grounded then
+        player:applyLinearImpulse(0, -3000)
+    end
+end
